8027f4a7bff79b0886b3038e710163edb80ba538
m_elhasan9 <mhmad201022@gmail.com>
2022-05-02 22:25:37 +0300

    test report with photo

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,4 @@ Route::post('user/login',[UserApiController::class,'login']);
 
 Route::middleware('auth:sanctum')->post('user/report',[UserApiController::class,'addReport']);
 Route::middleware('auth:sanctum')->post('user/addSubscribe',[UserApiController::class,'addSubscribe']);
+Route::middleware('auth:sanctum')->post('user/getSubscribes',[UserApiController::class,'getSubscribes']);
